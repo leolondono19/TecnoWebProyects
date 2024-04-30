310fe619a9e375d0a30f869b7a76eaee0efc91ec
EsauuR <165038332+EsauuR@users.noreply.github.com>
2024-04-30 18:56:43 -0400

    CONEXION A MYSQL EXITOSA
    PARA INICIAR npm start
    fijar el nombre en base de datos con root, contrasenia 1234, proyecto_tecnoweb, localhost. Para renombrar usuario y contrasena necesitamos usar: ALTER USER 'usuario_mysql'@'localhost' IDENTIFIED WITH mysql_native_password BY 'nueva_contraseña';

--- SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/server.cjs
+++ SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/server.cjs
@@ -1,21 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql'); // Importar el módulo mysql
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Ruta de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola desde la página de inicio!');
+// Configuración de la conexión a MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'proyecto_tecnoweb'
 });
 
-// Ruta de ejemplo para /api/message
-app.get('/api/message', (req, res) => {
-  res.json({ message: '¡Hola desde Express!' });
+// Conectar a la base de datos MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos MySQL establecida');
+});
+
+// Ruta de ejemplo para obtener datos de la base de datos
+app.get('/api/data', (req, res) => {
+  // Consulta SQL de ejemplo
+  const query = 'SELECT * FROM tabla_ejemplo';
+
+  // Ejecutar la consulta
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error al ejecutar la consulta' });
+      return;
+    }
+    // Enviar los resultados como respuesta
+    res.json(results);
+  });
 });
 
 app.listen(PORT, () => {
