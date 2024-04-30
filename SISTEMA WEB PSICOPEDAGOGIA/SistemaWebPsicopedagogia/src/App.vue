3367e441d2681baddd198d1522fe369ed0772a11
EsauuR <165038332+EsauuR@users.noreply.github.com>
2024-04-29 21:22:48 -0400

    Configurar Vue para consumir la API de Express:

--- SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/src/App.vue
+++ SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/src/App.vue
@@ -1,57 +1,53 @@
-  <template>
-    <div class="page-wrapper">
-        <LayoutHeader1 />
-        <div>
-          <router-view></router-view>
-
-        </div>
-        <LayoutFooter1 />
-
+<template>
+  <div class="page-wrapper">
+    <LayoutHeader1 />
+    <div>
+      <router-view></router-view>
     </div>
+    <LayoutFooter1 />
+  </div>
 </template>
-       
 
+<script>
+import axios from 'axios';
+import LayoutHeader1 from '@/components/layout/Header1.vue';
+import LayoutFooter1 from '@/components/layout/Footer1.vue';
+import ElementsBackToTop from '@/components/elements/BackToTop.vue';
 
- 
- <script>
- //import Cookies from 'js-cookie';
-  import LayoutHeader1 from '@/components/layout/Header1.vue';
-  import LayoutFooter1 from '@/components/layout/Footer1.vue';
-  import ElementsBackToTop from '@/components/elements/BackToTop.vue';
- export default {
-     name: 'App',
-     components: {
-       LayoutHeader1,
-       LayoutFooter1,
-        ElementsBackToTop
-     },
-    
-     data() {
-       return {
-         usuarioAutenticado: false,
-       };
-     },
-     methods: {
-        scrollToTop() {
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          });
-        },
-     },
-    
- };
- </script>
- 
- <style >
- body{
+export default {
+  name: 'App',
+  components: {
+    LayoutHeader1,
+    LayoutFooter1,
+    ElementsBackToTop
+  },
+  mounted() {
+    axios.get('/api/message')
+      .then(response => {
+        console.log(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error al obtener el mensaje:', error);
+      });
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
+
+<style>
+body {
   width: 100vw;
   overflow-x: hidden;
- }
+}
 
-
- #app{
+#app {
   max-width: 100vw;
- }
-
- </style>
+}
+</style>
