53af5122e1d7b98fd8e990819498e925c9217518
leolondono19 <leoneelondono@gmail.com>
2024-05-02 04:20:16 -0400

    final

--- SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/src/router/index.js
+++ SISTEMA WEB PSICOPEDAGOGIA/SistemaWebPsicopedagogia/src/router/index.js
@@ -1,6 +1,10 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/Login.vue'
+
+
+
 
     //Vista estudiantes
 import Docentes from '../views/Estudiante/Docentes.vue'    
@@ -19,8 +23,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/HomeView',
-      name: 'HomeView',
+      path: '/home',
+      name: 'home',
       component: HomeView
     },
     {
